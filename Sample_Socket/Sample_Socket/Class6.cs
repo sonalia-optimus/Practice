e85c2cd868f44292a8f328a31f55ca44ffb79ca0
sonali <sonaliagarwal1996@gmail.com>
2018-01-28 01:25:51 +0530

    class 6 addition

---
+++ Sample_Socket/Sample_Socket/Class6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Socket
+{
+   public  class Class6
+    {
+       public static  void Main(string[] Args)
+        {
+            int i = 8 + 7;
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
